33746f2d54caf9837bc96ba5ef039cb9e3377642
Yahya taouarda <yahyataouarda2004@gmail.com>
2025-07-10 20:27:04 +0100

    add readem.md file

---
+++ js/main.js
@@ -0,0 +1,11 @@
+const video1 = document.getElementById("projectvideo1");
+const video2 = document.getElementById("projectvideo2");
+const video3 = document.getElementById("projectvideo3");
+
+const videoList = [video1, video2, video3];
+
+videoList.forEach(function (video) {
+  video.addEventListener("mouseover", function () {
+    video.play();
+  });
+});
